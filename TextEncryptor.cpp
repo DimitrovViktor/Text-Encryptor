4e78998c17a463cf7eff620bab27c17ca0a1d503
Viktor <152012083+DimitrovViktor@users.noreply.github.com>
2025-12-15 18:37:53 +0000

    Add encryption and refactor password hashing

--- TextEncryptor.cpp
+++ TextEncryptor.cpp
@@ -1,46 +1,56 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 #include "argon/argon2.h"
-#include <string>
 
-void hashings(std::string encrPass);
+#include "libsodium/sodium.h"
 
-std::string encryptor(std::string encrKey, std::string normText);
+std::vector<unsigned char> hashings(std::string encrPass);
+
+std::string encryptor(std::string encrKey, std::string normText, std::vector<unsigned char> hash);
 
 std::string getHash(const char* encoded);
 
 int main()
 {
+    if (sodium_init() < 0) {
+        std::cout << "sodium didn't load";
+    }
     std::string encrKey;
     std::string normText;
     std::string encrText;
 
-    char hash[32];
-    char encoded[128];
+    std::string hashPass;
 
+    // Get password
     std::cout << "Enter password:\n";
     std::getline(std::cin, encrKey);
 
+    // Get password hash
+    std::vector<unsigned char> hash = hashings(encrKey);
+    
+
+    // Get text to encrypt
     std::cout << "Enter text:\n";
     std::getline(std::cin,normText);
 
     const char* charKey = normText.c_str();
 
-    encrText = encryptor(encrKey, normText);
+    // Encrypt text
+    encrText = encryptor(encrKey, normText, hash);
 
+    // Print encrypted text
     std::cout << "test text:\n" << encrText << "\n";
-    
-
-    hashings(normText);
 
     std::getline(std::cin, encrText);
 }
 
-void hashings(std::string encrPass) {
+std::vector<unsigned char> hashings(std::string encrPass) {
     const char* charKey = encrPass.c_str();
     const char* salt = "100_200_Salting"; // I will most likely also allow users to add custom salt
     // can also add peppering
-    char hash[32];
+    std::vector<unsigned char> hash(crypto_secretbox_KEYBYTES);
     char encoded[128];
     int result = argon2_hash(
         2,
@@ -48,7 +58,7 @@ void hashings(std::string encrPass) {
         1,
         charKey, strlen(charKey),
         salt, strlen(salt),
-        hash, sizeof(hash),
+        hash.data(), hash.size(),
         encoded, sizeof(encoded),
         Argon2_id,
         ARGON2_VERSION_13
@@ -62,6 +72,7 @@ void hashings(std::string encrPass) {
     else {
         std::cout << "hash failed: " << argon2_error_message(result) << std::endl;
     }
+    return hash;
 }
 
 std::string getHash(const char* encoded) {
@@ -73,10 +84,28 @@ std::string getHash(const char* encoded) {
     return encodedStr.substr(lastDollar + 1);
 }
 
-std::string encryptor(std::string encrKey, std::string normText) {
+std::string encryptor(std::string encrKey, std::string normText, std::vector<unsigned char> hash) {
     std::string encrText;
 
-    encrText = normText /*+ encryption*/;
+    // Generate nonce
+    unsigned char nonce[crypto_secretbox_NONCEBYTES];
+    randombytes_buf(nonce, sizeof nonce);
 
-    return encrText;
+    // Encrypt
+    std::vector<unsigned char> ciphertext(normText.size() + crypto_secretbox_MACBYTES);
+    crypto_secretbox_easy(ciphertext.data(),
+        (const unsigned char*)normText.data(), normText.size(),
+        nonce, hash.data());
+
+    // Add nonce + ciphertext for output
+    std::string resultStr((char*)nonce, sizeof nonce);
+    resultStr += std::string((char*)ciphertext.data(), ciphertext.size());
+
+    // Encode as base64 for display/storage
+    char b64[2048];
+    sodium_bin2base64(b64, sizeof b64,
+        (const unsigned char*)resultStr.data(), resultStr.size(),
+        sodium_base64_VARIANT_ORIGINAL);
+
+    return std::string(b64);
 }
